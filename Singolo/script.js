7d4aa0a300793a7dc8b9ed841b55db3a710e7a87
Anastasiya Cherniavskaya <nastya.che92@gmail.com>
2021-02-02 23:43:59 +0300

    update all files

--- Singolo/script.js
+++ Singolo/script.js
@@ -4,61 +4,24 @@ document.body.addEventListener('click', (e) => {
     if(clickedElement.classList.contains('menu-button')) {
         document.body.classList.toggle('menu-opened');
     }
-    else if(clickedElement.classList.contains('navigation__link')) {
-        console.log('hello');
-        document.body.classList.remove('active');
-        clickedElement.classList.add('active');
+    else if(clickedElement.classList.contains('navigation__link')) {                              
+        document.querySelector('.link--active').classList.remove('link--active');
+        clickedElement.classList.add('link--active');  
+    }
+    else if(clickedElement.classList.contains('phone-button')) {
+        clickedElement.parentNode.classList.toggle('phone-display-hiden'); 
     }
     else if(clickedElement.classList.contains('form__label')) {
-
+        removeImages ();
+        document.querySelector('.label--active').classList.remove('label--active');
+        clickedElement.classList.add('label--active');  
     }
 });
 
 const gallery = document.querySelector('.portfolio__images-gallery');
 
-replacedNode = gallery.replaceChild(gallery.children[1], gallery.children[0]);
-gallery.appendChild(replacedNode);
+function removeImages () {
+    replacedNode = gallery.replaceChild(gallery.children[1], gallery.children[0]);
+    gallery.appendChild(replacedNode);
+}
 
-
-
-// const menuButton = document.querySelector('.menu-button');
-
-// menuButton.addEventListener('click', () => {
-//     document.body.classList.toggle('menu-opened');
-// });
-
-// const links = document.querySelectorAll('.navigation__link');
-
-// links.forEach((link) =>
-//     link.addEventListener('click', () => {
-//         console.log('hello');
-//         links.forEach(link => link.classList.remove('active'));
-
-//         link.classList.add('active');
-//     })
-// );
-
-
-
-// const gallery = document.querySelector('.portfolio__images-gallery');
-// const inputButtons = document.querySelectorAll('.form__label');
-
-// inputButtons.forEach((inputButton) =>
-//     inputButton.addEventListener('click', () => {
-//         inputButtons.forEach(inputButton => inputButton.classList.remove('label--active'));
-
-//         inputButton.classList.add('label--active');
-//         replacedNode = gallery.replaceChild(gallery.children[1], gallery.children[0]);
-//         gallery.appendChild(replacedNode);
-//     })
-// );
-
-
-
-// window.onscroll = () => {
-//     if (window.scrollY > 300) {
-//         links.forEach(link => link.classList.add('scrolled'));
-//     } else {
-//         links.forEach(link => link.classList.remove('scrolled'));
-//     }
-// };
