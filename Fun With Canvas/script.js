dab4aec6735c636155eb18790af122bbed1615bd
Anastasiya Cherniavskaya <nastya.che92@gmail.com>
2021-04-08 19:11:50 +0300

    update js file, create clear function

--- Fun With Canvas/script.js
+++ Fun With Canvas/script.js
@@ -15,7 +15,7 @@ const CHANGE_TYPE_TO_INPUTS_MAP = {
     brush: '<input type="range" class="form__input input-range" min="6" max="30" value="6" name="brush" step="1" data-property="width">',
     palette: '<input type="color" class="form__input input-color" name="palette" value="#af2d93" data-property="color">',
     background: '<input type="color" class="form__input input-color input-background" name="background" value="#e0dae0" data-property="background">',
-    eraser: '<input type="range" class="form__input input-range" min="3" max="30" value="3" name="eraser" step="1" data-property="width">',
+    eraser: '<input type="range" class="form__input input-range" min="3" max="30" value="3" name="eraser" step="1" data-property="heigth">',
 };
 
 canvas.width = (getComputedStyle(canvas.parentNode).width).slice(0, -2);
@@ -23,18 +23,21 @@ canvas.height = (getComputedStyle(canvas.parentNode).height).slice(0, -2);
 ctx.lineCap = 'round';
 ctx.lineCap = 'round';
 
+let buttonName = null;   // new
+let eraserValue = null;    // new
+
 paintEditor.addEventListener('click', (e) => {
     const clickedElement = e.target.closest('.paint__button');
+    
+    if(clickedElement) {   
+        const changeType = clickedElement.dataset.changeType;
 
-    if(clickedElement) {                 
         paintEditor.querySelector('.paint__button--active').classList.remove('paint__button--active');
         clickedElement.classList.add('paint__button--active');  
+        paintForm.classList.add('paint__form--opened');
+        paintForm.innerHTML = CHANGE_TYPE_TO_INPUTS_MAP[changeType];
 
-        if(clickedElement.dataset.changeType) {
-            const changeType = clickedElement.dataset.changeType;
-            paintForm.classList.add('paint__form--opened');
-            paintForm.innerHTML = CHANGE_TYPE_TO_INPUTS_MAP[changeType];
-        }
+        buttonName = changeType;  // new
     }else if(!clickedElement && !e.target.classList.contains('form__input')) {
         paintEditor.querySelector('.paint__form--opened').classList.remove('paint__form--opened');
     }
@@ -45,7 +48,7 @@ paintForm.addEventListener('change', (e) => {
         const propValue = e.target.value;
         const propName = e.target.dataset.property;
 
-        changeProperty(propName, propValue);
+        buttonName == 'eraser' ? eraserValue = propValue : changeProperty(propName, propValue);
         changeInputValue(e); 
     }
 });
@@ -57,7 +60,9 @@ canvas.addEventListener('mouseup', finish);
 function changeProperty(propName, propValue) {
     if(propName == 'background') {
         canvas.parentNode.style.setProperty(`--${propName}`, propValue);
-    }else 
+    }else if(propName == 'eraser') {  //new change
+        eraserValue = propValue; //new
+    }else
         picture[propName] = propValue;
 };
 
@@ -74,22 +79,26 @@ function start(e) {
     if(paintForm.classList.contains('paint__form--opened')) {
         paintForm.classList.remove('paint__form--opened');
     }
-    picture.drawing = true;
-    ctx.beginPath();
-    ctx.moveTo(e.clientX - position.left, e.clientY - position.top);
-
-    e.preventDefault();
+        picture.drawing = true;
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - position.left, e.clientY - position.top);
+    
+        e.preventDefault();
 };
 
 function draw(e) {
     if( picture.drawing === true) {
-        ctx.strokeStyle = picture.color;
-        ctx.lineWidth = picture.width;
-        ctx.lineTo(e.clientX - position.left, e.clientY - position.top);
-        ctx.stroke();     
+        if(buttonName == 'eraser') {   // new
+            clear(e);
+        }else {
+            ctx.strokeStyle = picture.color;
+            ctx.lineWidth = picture.width;
+            ctx.lineTo(e.clientX - position.left, e.clientY - position.top);
+            ctx.stroke();  
+        }
     }
     e.preventDefault();
-}
+};
 
 function finish(e) {
     if( picture.drawing === true) {
@@ -100,9 +109,7 @@ function finish(e) {
     e.preventDefault();
 };
 
-// function clear(e) {
-//     if( picture.drawing === false) {
-//         ctx.clearRect(e.clientX - position.left, e.clientY - position.top, e.target.value, e.target.value);
-//     } 
-// };
+function clear(e) {   // new
+        ctx.clearRect(e.clientX - position.left - (eraserValue / 2), e.clientY - position.top - (eraserValue / 2), eraserValue, eraserValue);
+};
 
