40bfbbf2bd154bc1c00ce89bc773937e5b2858e0
Anastasiya Cherniavskaya <nastya.che92@gmail.com>
2021-05-23 13:58:54 +0300

    refactor: change structure of the project

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -1,28 +1,29 @@
 const keyBoard = document.querySelector('.keyboard');
 const audio = document.querySelectorAll('.key__audio');
 
-keyBoard.addEventListener('click', (e) => {
-    const clickedElement = e.target;
+keyBoard.addEventListener('click', (event) => {
+    const clickedElement = event.target;
     const button = clickedElement.closest('.keyboard__key');
 
-    if(button) {
+    if (button) {
         const buttonName = button.dataset.name;
+
         playAudio(buttonName);
     }
 });
 
 function playAudio(buttonName) {
     audio.forEach(audioSound => {
-        if(audioSound.dataset.name == buttonName) {
+        if (audioSound.dataset.name == buttonName) {
             audioSound.play();
         };
     })
 };
 
-document.addEventListener('keydown', (e) => {
-    const buttonName = e.code.slice(3).toLocaleLowerCase();
+document.addEventListener('keydown', (event) => {
+    const buttonName = event.code.slice(3).toLocaleLowerCase();
 
-    if(e.code) {
+    if (event.code) {
        playAudio(buttonName);
     }
 });
