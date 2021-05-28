0d05d90b2ce13179519d5141ffd42502be983e94
Anastasiya Cherniavskaya <nastya.che92@gmail.com>
2021-05-28 23:46:27 +0300

    refactor: update js file

--- Whack_A_Mole/script.js
+++ Whack_A_Mole/script.js
@@ -5,7 +5,6 @@ const playingField = document.querySelector('.game__plaing-field');
 const finalScore = document.querySelector('.helper__score');
 
 let lastHole = null;
-let {clickOnMole, timeUp} = false;
 let score = 0;
 
 const holesArray = Array.prototype.slice.call(holes);
@@ -33,7 +32,7 @@ function showRandomMole() {
     
     setTimeout(() => {
         hole.classList.remove('plaing-field__unit--activated');
-        if(!timeUp) {
+        if (!timeUp) {
             showRandomMole();
         }
     }, time);
@@ -57,7 +56,7 @@ function startGame() {
 };
 
 function finishGame() {
-    if(timeUp) {
+    if (timeUp) {
         game.classList.remove('game--started');
         game.classList.add('game--finished');
         finalScore.textContent = score > 0 ? `${score} points` : `${score} point`;
@@ -69,14 +68,14 @@ function finishGame() {
 document.addEventListener('click', (event) => {
     const clickedElement = event.target;
 
-    if(clickedElement.classList.contains('game__button-start') && !game.classList.contains('game--started') && !game.classList.contains('game--finished')) {
+    if (clickedElement.classList.contains('game__button-start') && !game.classList.contains('game--started') && !game.classList.contains('game--finished')) {
         game.classList.add('game--started');
         startGame();
-    }if(clickedElement.closest('.plaing-field__mole') && !clickOnMole) {
+    } if (clickedElement.closest('.plaing-field__mole')) {
         score++;
         scoreSpan.textContent = score;
         createScoreElement(clickedElement);
-    }if(clickedElement.classList.contains('helper__button')) {
+    } if (clickedElement.classList.contains('helper__button')) {
         game.classList.remove('game--finished');
         timeUp = false;
     }
